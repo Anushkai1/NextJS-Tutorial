eda6f5e493aadb658af510d5d044acc7410db06d
Anushka Isuru <129645615+AnushkaIsuru1@users.noreply.github.com>
2024-04-21 15:42:02 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
+import Navbar from "./Components/Navbar/Navbar";
 
 const Home = async ()=> {
 
 
   return (
     <>
-
-      
+    <Navbar/>      
     </>
   );
 }
