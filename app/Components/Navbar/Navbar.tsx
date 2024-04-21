3beee92319f5565a4a0bf7aeb49282c1d78bc0fd
Anushka Isuru <129645615+AnushkaIsuru1@users.noreply.github.com>
2024-04-21 15:41:54 +0530

    Create Navbar.tsx

---
+++ app/Components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Navbar.module.css'
+
+function Navbar() {
+  return (
+    <div className={styles.nav}>Navbar</div>
+  )
+}
+
+export default Navbar
