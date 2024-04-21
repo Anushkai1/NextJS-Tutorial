3df91819334c559d6e06b15208534fe45477b276
Anushka Isuru <129645615+AnushkaIsuru1@users.noreply.github.com>
2024-04-21 12:09:13 +0530

    Create first component

---
+++ app/Components/ProductCart.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CardBotton from './CardBotton'
+
+function ProductCart() {
+    return (
+        <>
+            <h2>ProductCart</h2>
+           
+        </>
+
+    )
+}
+
+export default ProductCart
