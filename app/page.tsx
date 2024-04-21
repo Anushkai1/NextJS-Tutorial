b9f8921ad93a352820f5753cf5adb37c9bc7db33
Anushka Isuru <129645615+AnushkaIsuru1@users.noreply.github.com>
2024-04-21 15:33:27 +0530

    Clear

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,11 @@
 import Image from "next/image";
 
-interface UseriInterface{
-  id:number,
-  name: string
-}
 const Home = async ()=> {
 
-  const res = await fetch('https://jsonplaceholder.typicode.com/users', {next : {revalidate : 10}})// 10 seconds
-  const users : UseriInterface[] = await res.json()
+
   return (
     <>
-    <ul>
-     {users.map(user => <li key= {user.id}>{user.name}</li> )}
-    </ul>
+
       
     </>
   );
