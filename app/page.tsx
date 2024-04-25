8e5dbbeb81779b61abdbbd16208bf8e90a217ebf
Anushka Isuru <129645615+AnushkaIsuru1@users.noreply.github.com>
2024-04-25 17:16:01 +0530

    Animate on Scroll

--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,30 @@
 import React from 'react'
 import { motion } from "framer-motion"
 
-
 function Home() {
 
   return (
-    <div >
+    <div>
+      <div className='relative h-svh w-full bg-blue-600 p-10' style={{ height: '300vh' }}>a</div>
 
-      <motion.div
-        variants={{
-          hidden: { opacity: 0 },
-          visible: { opacity: 1 }
-        }}
-        initial="hidden"
-        whileInView="visible"
-        transition={{ duration: .3, delay: .2 }}
-      >
+      <div className='relative overflow-hidden'>
 
-        <div className='relative h-svh w-full bg-red-600'>a</div>
+        <motion.div
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 }
+          }}
+          initial="hidden"
+          whileInView="visible"
+          transition={{ duration: 1, delay: .2 }}
+        >
 
-      </motion.div>
+          <div className='relative h-svh w-full bg-red-600'>a</div>
+        </motion.div>
+        
+      </div>
     </div>
+
   )
 }
 
