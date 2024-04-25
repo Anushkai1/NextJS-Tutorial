bc55b9a3290c781e5fd7080ada2b068e7804599c
Anushka Isuru <129645615+AnushkaIsuru1@users.noreply.github.com>
2024-04-25 17:01:16 +0530

    Framer motion 1st Animtion

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,28 @@
-import Image from "next/image";
-import Navbar from "./Components/Navbar/Navbar";
+'use client'
+import React from 'react'
+import { motion } from "framer-motion"
 
-const Home = async () => {
 
+function Home() {
 
   return (
-    <>
-      <Image
-        priority={true}
-        fill={true}
-        // blurDataURL=''
-        // placeholder="blur"
-        src="/example.png"
-        sizes="(max-width: 768px) 50vw, (max-width: 1200px) 30vw, 33vw"
-        alt={""} />
-    </>
-  );
-}
+    <div >
 
+      <motion.div
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1 }
+        }}
+        initial="hidden"
+        whileInView="visible"
+        transition={{ duration: .3, delay: .2 }}
+      >
+
+        <div className='relative h-svh w-full bg-red-600'>a</div>
+
+      </motion.div>
+    </div>
+  )
+}
 
 export default Home
